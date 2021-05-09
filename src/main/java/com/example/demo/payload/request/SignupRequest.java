579adf2cbb9cd59ab582d09c57041acae18b8f85
unknown <tasabina@yandex.ru>
2021-05-09 16:06:43 +1200

    Finished with user authentication

---
+++ src/main/java/com/example/demo/payload/request/SignupRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo.payload.request;
+
+import com.example.demo.annotation.PasswordMatches;
+import com.example.demo.annotation.ValidEmail;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@PasswordMatches
+public class SignupRequest {
+    @Email(message = "It should have email format")
+    @NotBlank(message = "User email is required")
+    @ValidEmail
+    private String email;
+    @NotBlank(message = "Please enter your name")
+    private String firstname;
+    @NotBlank(message = "Please enter your lastname")
+    private String lastname;
+    @NotBlank(message = "Please enter your username")
+    private String username;
+    @NotBlank(message = "Password is required")
+    @Size(min=6)
+    private String password;
+    private String confirmPassword;
+}
---
+++ src/main/java/com/example/demo/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.payload.response;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidLoginResponse {
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+    }
+
+
+}
